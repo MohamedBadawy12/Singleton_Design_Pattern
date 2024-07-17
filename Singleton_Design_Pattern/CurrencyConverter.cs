6e417ca2c2d3e7cd6f3600b673ced5c435ab494f
MohamedBadawy12 <m.badawy2442002@gmail.com>
2024-07-18 00:00:47 +0300

    Add project files.

---
+++ Singleton_Design_Pattern/CurrencyConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace Singleton_Design_Pattern
+{
+    public class CurrencyConverter
+    {
+        private static CurrencyConverter _instance;
+        private Dictionary<string, decimal> _exchangeRates;
+        private static readonly object _locker = new object();
+        public CurrencyConverter()
+        {
+            _exchangeRates = new Dictionary<string, decimal>
+            {
+                {"USD", 1.0m},
+                {"EUR", 0.85m},
+                {"GBP", 0.75m},
+                {"JPY", 110.0m}
+            };
+        }
+        public static CurrencyConverter GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new CurrencyConverter();
+                    }
+                }
+            }
+            return _instance;
+        }
+        public decimal Convert(string fromCurrency, string toCurrency, decimal amount)
+        {
+            if (!_exchangeRates.ContainsKey(fromCurrency) || !_exchangeRates.ContainsKey(toCurrency))
+            {
+                throw new ArgumentException("Invalid currency code.");
+            }
+            decimal fromRate = _exchangeRates[fromCurrency];
+            decimal toRate = _exchangeRates[toCurrency];
+            return (amount / fromRate) * toRate;
+        }
+    }
+}
---
+++ Singleton_Design_Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton_Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CurrencyConverter converter = CurrencyConverter.GetInstance();
+                Console.Write("Enter your currency do you want to convert: ");
+                decimal amountCurreny = decimal.Parse(Console.ReadLine());
+                Console.Write("Convert From: ");
+                string currency1 = Console.ReadLine();
+                Console.Write("Convert To: ");
+                string currency2 = Console.ReadLine();
+                decimal amount = converter.Convert(currency1, currency2, amountCurreny);
+                Console.WriteLine($"{amountCurreny} {currency1} is {amount} {currency2}");
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+    }
+}
